e558f502267577c973bf33359c4e7268a0f9b655
parisfuchsia <liammagat8.gmail.com>
2025-01-22 09:26:06 +0800

    Changed envs

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,15 @@ const connectDb = require("./config/index.js");
 const router = require("./router/router.js");
 dotenv.config();
 
+
+
 const app = express();
 app.use(express.json());
 app.use(cors({
   origin: process.env.ORIGIN_URL,
   credentials: true
 }));
+
 app.use(cookieParser());
 
 app.use("/", router);
@@ -19,9 +22,7 @@ app.use("/", router);
 const PORT = process.env.SECRET_PORT;
 
 connectDb().then(() => {
-  app.listen(PORT, () => {
-    
-  });
+  
 }).catch(() => {
   
 })
